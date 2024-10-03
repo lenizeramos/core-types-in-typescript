6c4bfe58b18b745e83188c2aa5512de92fe53913
lenizeramos <lenizemr@gmail.com>
2024-10-02 22:57:40 -0700

    add node modules to gitignore

---
+++ exercises/ex01.js
@@ -0,0 +1,7 @@
+var reservation = {
+    customerInformation: "Lulu",
+    paymentInformation: true,
+    checkIn: 15,
+    checkOut: 11,
+};
+console.log(reservation);
--- exercises/ex01.ts
+++ exercises/ex01.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+const reservation: {
+    customerInformation: string,
+    paymentInformation: boolean,
+    checkIn: number,
+    checkOut: number,
+} = {
+    customerInformation: "Lulu",
+    paymentInformation: true,
+    checkIn: 15,
+    checkOut: 11,
+}
+
+console.log(reservation);
